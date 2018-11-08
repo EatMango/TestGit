8582f457eb5ce2f6310c4cde3852ca30f5f496fe
zhaoxiaoyu <zhaoxiaoyu@qiyi.com>
2018-11-08 10:16:33 +0800

    测试-11.7

---
+++ src/link/Html.java
@@ -0,0 +1,13 @@
+package link;
+
+import javax.swing.text.Document;
+import java.io.IOException;
+
+public class Html {
+    public Document getHtmlTextByUrl(){
+        Document document =null;
+        int i= (int) (Math.random()*1000);
+
+        return document;
+    }
+}
